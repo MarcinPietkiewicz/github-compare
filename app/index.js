64935d02e3f48951d4d3a12a275679d93cbfee58
Marcin <marcin.p.dev@gmail.com>
2021-06-08 17:33:16 +0200

    Introduced compare component to compare github users

--- app/index.js
+++ app/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Popular from './components/Popular'
+import Compare from './components/Compare'
 
 class App extends React.Component{
     render(){
         return (
         <div className='container'>
-            <Popular />
+            <Compare />
         </div>
         )}
 }
