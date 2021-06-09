0b921072c9b673a7252f3e09d86c3d819b734bb8
Marcin <marcin.p.dev@gmail.com>
2021-06-09 23:42:08 +0200

    Added players input to compare screen

--- app/components/Compare.js
+++ app/components/Compare.js
@@ -88,12 +88,52 @@ PlayerInput.propTypes = {
 }
 
 export default class Compare extends React.Component {
+    constructor(props){
+        super(props)
+
+this.state = {
+    playerOne: null,
+    playerTwo: null,
+    }
+
+this.handleSubmit = this.handleSubmit.bind(this);
+}
+
+handleSubmit(id, player){
+    this.setState({
+        [id]: player
+    })
+}
+
+
     render(){
+
+        const {playerOne, playerTwo} = this.state
+
+
         return (
             <React.Fragment>
                  <Instructions />   
 
-                 <PlayerInput label="label" onSubmit={value => console.log('value!', value)}/>
+                <div>
+                    <h1 className='center-text header-lg'>Players</h1>
+                </div>
+                <div className="row space-around">
+                {playerOne === null && (
+                    <PlayerInput    
+                        label="Player One"
+                        onSubmit={(player) => this.handleSubmit('playerOne', player)}
+                    />
+                )}
+                 {playerTwo === null && (
+                    <PlayerInput    
+                        label="Player Two"
+                        onSubmit={(player) => this.handleSubmit('playerTwo', player)}
+                    />
+                )}
+                </div>
+
+                 {/* <PlayerInput label="label" onSubmit={value => console.log('value!', value)}/> */}
 
             </React.Fragment>
 
