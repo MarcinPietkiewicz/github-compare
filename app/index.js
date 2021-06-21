33d5c33750d3cd89b4a0610534949581aabfe7b6
Marcin <marcin.p.dev@gmail.com>
2021-06-21 13:10:58 +0200

    Introduced theme context

--- app/index.js
+++ app/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Popular from './components/Popular'
 import Compare from './components/Compare'
+import {ThemeProvider} from './contexts/theme'
 
 class App extends React.Component{
     render(){
         return (
-        <div className='container'>
-            <Popular />
-        </div>
+        <ThemeProvider value={}>
+            <div className='container'>
+                <Popular />
+            </div>
+        </ThemeProvider>
         )}
 }
 
