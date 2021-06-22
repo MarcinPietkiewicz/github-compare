64999044f7871f7a17828689efbcf97d92ecdd51
Marcin <marcin.p.dev@gmail.com>
2021-06-22 11:45:43 +0200

    Fixed results component for themes

--- app/components/Results.js
+++ app/components/Results.js
@@ -6,62 +6,9 @@ import PropTypes from 'prop-types'
 import Loading from './Loading'
 import Tooltip from './Tooltip'
 
-const styles = {
-  container: {
-    position: 'relative',
-    display: 'flex'
-  },
-  tooltip: {
-    boxSizing: 'border-box',
-    position: 'absolute',
-    width: '160px',
-    bottom: '100%',
-    left: '50%',
-    marginLeft: '-80px',
-    borderRadius: '3px',
-    backgroundColor: 'hsla(0, 0%, 20%, 0.9)',
-    padding: '7px',
-    marginBottom: '5px',
-    color: '#fff',
-    textAlign: 'center',
-    fontSize: '14px',
-  }
-}
-
-class ProfileList extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      hoveringLocation: false,
-      hoveringCompany: false,
-    }
-
-    this.mouseOver = this.mouseOver.bind(this)
-    this.mouseOut = this.mouseOut.bind(this)
-
-  }
-  
-  mouseOver(id){
-    this.setState({
-      [id]: true
-    })
-  }
-  
-  mouseOut(id) {
-    this.setState({
-      [id]: false
-    })
-  }
-  
-
-
-  render () {
-    const {profile} = this.props
-    const { hoveringCompany, hoveringLocation} = this.state
-
-    return (
-      <ul className='card-list'>
+function ProfileList ({ profile }) {
+  return (
+    <ul className='card-list'>
       <li>
         <FaUser color='rgb(239, 115, 115)' size={22} />
         {profile.name}
@@ -77,8 +24,8 @@ class ProfileList extends React.Component {
       {profile.company && (
         <li>
           <Tooltip text="User's company">
-          <FaBriefcase color='#795548' size={22} />
-          {profile.company}
+            <FaBriefcase color='#795548' size={22} />
+            {profile.company}
           </Tooltip>
         </li>
       )}
@@ -91,9 +38,9 @@ class ProfileList extends React.Component {
         {profile.following.toLocaleString()} following
       </li>
     </ul>
-    )
-  }
+  )
 }
+  
 
 
 
